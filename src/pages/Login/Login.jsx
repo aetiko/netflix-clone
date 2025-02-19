068e1f91ca9ac9ce36f11665dc27a5bb3f080afe
ayo <nurudeenetiko12@gmail.com>
2025-02-19 16:12:45 -0700

    sign up/ sign in page in progress

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
-    <div className='login'></div>
+    <div className='login'>
+      <img src={logo} alt="" className='login_logo'/>
+      <div className="login_form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Name' />
+          <input type="email" placeholder='Email-address' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="form_help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
