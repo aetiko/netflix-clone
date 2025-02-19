d4b66f048b0a744196a2e1a63b701487550cf3e2
ayo <nurudeenetiko12@gmail.com>
2025-02-19 15:39:55 -0700

    adding the footer icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import 'Footer.css'
+import './Footer.css'
+import yotube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
 
 const Footer = () => {
   return (
-    <div className='footer'></div>
+    <div className='footer'>
+      <div className="footer_icons">
+        <img src={facebook_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={yotube_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
+
+
   const cardsRef = useRef();
 
   const handleWheel = (event)=>{
@@ -16,7 +18,7 @@ const TitleCards = () => {
 
   return (
     <div className="title_cards">
-      <h2>Popular on Netflix</h2>
+      <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card_list" ref={cardsRef}>
         {cards_data.map((card, index) => {
           return (
